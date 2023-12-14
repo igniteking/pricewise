89ba1965cc8c2847cbeba8fdd106e11b359d3632
igniteking <khanzaidan786@gmail.com>
2023-12-14 14:28:49 +0530

    maxDuration fixed

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,7 @@ import {
 } from "@/lib/utils/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 10; // This function can run for a maximum of 300 seconds
+export const maxDuration = 1; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
